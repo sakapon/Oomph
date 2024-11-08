3d0ce34f7adbb28317041d1f4d81f6d92caf02b7
Keiho Sakapon <git@saka-pon.net>
2024-11-08 21:56:59 +0900

    Add interfaces

---
+++ CSharp/Oomph.Data/UF09Lib/UFs/IFs/IIntUnionFind.cs
@@ -0,0 +1,9 @@
+﻿namespace Oomph.Data.UF09Lib.UFs.IFs
+{
+	public interface IIntUnionFind
+	{
+		public int Find(int x);
+		public bool Union(int x, int y);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Lib/UFs/IFs/IUnionFind.cs
@@ -0,0 +1,10 @@
+﻿namespace Oomph.Data.UF09Lib.UFs.IFs
+{
+	public interface IUnionFind<TKey>
+	{
+		public class Node { }
+		public Node Find(TKey x);
+		public bool Union(TKey x, TKey y);
+		public bool AreSame(TKey x, TKey y) => Find(x) == Find(y);
+	}
+}
