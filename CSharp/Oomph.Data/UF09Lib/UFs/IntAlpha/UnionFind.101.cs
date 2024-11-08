1930b033e9b09de2022de472034e3f0c879a244c
Keiho Sakapon <git@saka-pon.net>
2024-11-08 22:11:34 +0900

    Add UnionFind 101

---
+++ CSharp/Oomph.Data/UF09Lib/UFs/IntAlpha/UnionFind.101.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// 何も工夫しない実装
+// O(n)
+namespace Oomph.Data.UF09Lib.UFs.V101
+{
+	public class UnionFind
+	{
+		readonly int[] parents;
+
+		public UnionFind(int n)
+		{
+			parents = new int[n];
+			Array.Fill(parents, -1);
+		}
+
+		public int Find(int x) => parents[x] == -1 ? x : Find(parents[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+
+		public bool Union(int x, int y)
+		{
+			if ((x = Find(x)) == (y = Find(y))) return false;
+
+			parents[y] = x;
+			return true;
+		}
+	}
+}
