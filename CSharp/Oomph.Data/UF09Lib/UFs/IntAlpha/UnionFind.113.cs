7b9480ec4347a1567074828ad723ff541abdd4af
Keiho Sakapon <git@saka-pon.net>
2024-11-09 12:18:59 +0900

    Add classes for union by rank

---
+++ CSharp/Oomph.Data/UF09Lib/UFs/IntAlpha/UnionFind.113.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// path compression, union by rank
+// O(α(n))
+namespace Oomph.Data.UF09Lib.UFs.v113
+{
+	public class UnionFind
+	{
+		readonly int[] parents, ranks;
+
+		public UnionFind(int n)
+		{
+			parents = new int[n];
+			Array.Fill(parents, -1);
+			ranks = new int[n];
+		}
+
+		public int Find(int x) => parents[x] == -1 ? x : parents[x] = Find(parents[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+
+		public bool Union(int x, int y)
+		{
+			if ((x = Find(x)) == (y = Find(y))) return false;
+
+			if (ranks[x] < ranks[y]) (x, y) = (y, x);
+			parents[y] = x;
+			if (ranks[x] == ranks[y]) ++ranks[x];
+			return true;
+		}
+	}
+}
