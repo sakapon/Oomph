1c686d8808f8108c4b6f77b9b78283b2a6625499
Keiho Sakapon <git@saka-pon.net>
2024-11-09 12:59:35 +0900

    Update code

--- CSharp/Oomph.Data/UF09Lib/UFs/IntBeta/UF.211.cs
+++ CSharp/Oomph.Data/UF09Lib/UFs/IntBeta/UF.211.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Linq;
 
-// short, path compression
+// short, path compression, from 101
+// O(log n)
 
 namespace Oomph.Data.UF09Lib.UFs.v211
 {
 	class UF
 	{
 		int[] p;
-		public UF(int n) => p = Enumerable.Repeat(-1, n).ToArray();
+		public UF(int n) { p = new int[n]; Array.Fill(p, -1); }
 		public int Find(int x) => p[x] == -1 ? x : p[x] = Find(p[x]);
 		public bool AreSame(int x, int y) => Find(x) == Find(y);
 		public void Union(int x, int y) { if ((x = Find(x)) != (y = Find(y))) p[y] = x; }
