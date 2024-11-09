c516ed736431589af7a55df7fb469978644affab
Keiho Sakapon <git@saka-pon.net>
2024-11-09 12:30:11 +0900

    Add class

---
+++ CSharp/Oomph.Data/UF09Lib/UFs/IntAlpha/UnionFind.123.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// path compression, union by size
+// parents[root] = -size
+// O(α(n))
+namespace Oomph.Data.UF09Lib.UFs.v123
+{
+	public class UnionFind
+	{
+		readonly int[] parents;
+
+		public UnionFind(int n)
+		{
+			parents = new int[n];
+			Array.Fill(parents, -1);
+		}
+
+		public int Find(int x) => parents[x] < 0 ? x : parents[x] = Find(parents[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+		public int GetGroupSize(int x) => -parents[Find(x)];
+
+		public bool Union(int x, int y)
+		{
+			if ((x = Find(x)) == (y = Find(y))) return false;
+
+			if (parents[x] > parents[y]) (x, y) = (y, x);
+			parents[x] += parents[y];
+			parents[y] = x;
+			return true;
+		}
+	}
+}
