18d5d66a32b1f84f97fc2c2b5d2244001bda305c
Keiho Sakapon <git@saka-pon.net>
2024-11-12 17:41:13 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/v401/ABC277_C.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v401;
+
+namespace UF09Test.UFs.v401
+{
+	// Test: https://atcoder.jp/contests/abc277/tasks/abc277_c
+	class ABC277_C
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int u, int v) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n], _ => Read2());
+
+			var keys = es.Select(e => e.u).Concat(es.Select(e => e.v)).Append(1);
+			var uf = new UnionFind<int>(keys);
+			foreach (var (a, b) in es)
+			{
+				uf.Union(a, b);
+			}
+			return uf.ToGroups()[uf.Find(1)].Max();
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/v401/ABC285_D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v401;
+
+namespace UF09Test.UFs.v401
+{
+	// Test: https://atcoder.jp/contests/abc285/tasks/abc285_d
+	class ABC285_D
+	{
+		static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+		static bool Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n], _ => Console.ReadLine().Split());
+
+			var keys = es.SelectMany(e => e);
+			var uf = new UnionFind<string>(keys);
+			foreach (var e in es)
+			{
+				if (!uf.Union(e[0], e[1])) return false;
+			}
+			return true;
+		}
+	}
+}
