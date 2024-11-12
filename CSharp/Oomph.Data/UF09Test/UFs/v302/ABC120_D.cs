0ba11d80730f48eb59475409e30b05513c34987d
Keiho Sakapon <git@saka-pon.net>
2024-11-12 11:58:47 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/v302/ABC120_D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v302;
+
+namespace UF09Test.UFs.v302
+{
+	// Test: https://atcoder.jp/contests/abc120/tasks/abc120_d
+	class ABC120_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var r = new List<long>();
+			var s = 0L;
+			var uf = new UnionFind(n + 1);
+
+			foreach (var (a, b) in es.Reverse())
+			{
+				uf.Union(a, b);
+			}
+
+			for (int j = 0; j < m; j++)
+			{
+				if (uf.Undo(out var a, out var b))
+				{
+					s += (long)uf.Find(a).Size * uf.Find(b).Size;
+				}
+				r.Add(s);
+			}
+			return string.Join("\n", r);
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/v302/ABC229_E.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v302;
+
+namespace UF09Test.UFs.v302
+{
+	// Test: https://atcoder.jp/contests/abc229/tasks/abc229_e
+	class ABC229_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int u, int v) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var map = es.ToLookup(e => e.u, e => e.v);
+
+			var r = new List<int>();
+			var uf = new UnionFind(n + 1);
+
+			for (int v = n; v > 0; v--)
+			{
+				foreach (var nv in map[v])
+				{
+					uf.Union(v, nv);
+				}
+			}
+
+			for (int v = 1; v <= n; v++)
+			{
+				foreach (var _ in map[v])
+				{
+					uf.Undo();
+				}
+
+				r.Add(uf.GroupsCount - v - 1);
+			}
+			return string.Join("\n", r);
+		}
+	}
+}
