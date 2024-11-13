799a99942eaf4d6a17058dab20013445f795a610
Keiho Sakapon <git@saka-pon.net>
2024-11-13 16:10:56 +0900

    Add UnionFind class with data augmentation

--- CSharp/Oomph.Data/UF09Lib/UFs/IntBeta/UnionFind.221.cs
+++ CSharp/Oomph.Data/UF09Lib/UFs/IntBeta/UnionFind.221.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-// int vertexes
+// int vertexes, data augmentation (relative), from 301
 
 namespace Oomph.Data.UF09Lib.UFs.v221
 {
@@ -13,7 +13,9 @@ namespace Oomph.Data.UF09Lib.UFs.v221
 			public int Key { get; internal set; }
 			internal Node Parent;
 			public int Size { get; internal set; } = 1;
-			public override string ToString() => Parent == null ? $"{Key}, Size = {Size}" : $"{Key} (not root)";
+			// 親を基準とした相対値
+			internal long Value;
+			public override string ToString() => Parent == null ? $"{Key}, Size = {Size}, Value = {Value}" : $"{Key} (not root), Value = {Value}";
 		}
 
 		readonly Node[] nodes;
@@ -30,20 +32,34 @@ namespace Oomph.Data.UF09Lib.UFs.v221
 			GroupsCount = n;
 		}
 
-		Node Find(Node n) => n.Parent == null ? n : n.Parent = Find(n.Parent);
+		Node Find(Node n)
+		{
+			if (n.Parent == null) return n;
+
+			var r = Find(n.Parent);
+			n.Value += n.Parent.Value;
+			return n.Parent = r;
+		}
+
 		public Node Find(int x) => Find(nodes[x]);
 		public bool AreSame(int x, int y) => Find(x) == Find(y);
 
-		public bool Union(int x, int y)
+		public bool Union(int x, int y, long x2y)
 		{
 			var nx = Find(x);
 			var ny = Find(y);
 			if (nx == ny) return false;
 
-			if (nx.Size < ny.Size) (nx, ny) = (ny, nx);
+			if (nx.Size < ny.Size)
+			{
+				(nx, ny) = (ny, nx);
+				(x, y) = (y, x);
+				x2y = -x2y;
+			}
 			ny.Parent = nx;
 			nx.Size += ny.Size;
 			--GroupsCount;
+			ny.Value = -nodes[y].Value + x2y + nodes[x].Value;
 			United?.Invoke(nx.Key, ny.Key);
 			return true;
 		}
@@ -51,5 +67,12 @@ namespace Oomph.Data.UF09Lib.UFs.v221
 		// 根とサイズの情報のみを取得します。
 		public Node[] GetGroupInfoes() => Array.FindAll(nodes, n => n.Parent == null);
 		public ILookup<Node, int> ToGroups() => nodes.ToLookup(Find, n => n.Key);
+
+		public long GetX2Y(int x, int y)
+		{
+			if (!AreSame(x, y)) throw new InvalidOperationException($"{x} and {y} are not in the same set.");
+			return nodes[y].Value - nodes[x].Value;
+		}
+		public bool Verify(int x, int y, long x2y) => AreSame(x, y) && nodes[y].Value == x2y + nodes[x].Value;
 	}
 }
