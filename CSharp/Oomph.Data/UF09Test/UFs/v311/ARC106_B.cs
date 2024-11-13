367ad554dc1d15ede31768878eaf45f57cc2fcf9
Keiho Sakapon <git@saka-pon.net>
2024-11-13 18:42:58 +0900

    Add test using 311

---
+++ CSharp/Oomph.Data/UF09Test/UFs/v311/ARC106_B.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v311;
+
+namespace UF09Test.UFs.v311
+{
+	// Test: https://atcoder.jp/contests/arc106/tasks/arc106_b
+	class ARC106_B
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+		static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+		static bool Solve()
+		{
+			var (n, m) = Read2();
+			var a = ReadL();
+			var b = ReadL();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var iv = a.Zip(b).ToArray();
+			var uf = new UnionFind<(long a, long b)>(n, (x, y) => (x.a + y.a, x.b + y.b), false, iv);
+			foreach (var (c, d) in es)
+			{
+				uf.Union(c - 1, d - 1);
+			}
+			return uf.GetGroupInfoes().All(g => g.Value.a == g.Value.b);
+		}
+	}
+}
