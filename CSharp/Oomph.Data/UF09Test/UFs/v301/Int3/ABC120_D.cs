1aae4890cd5cfac53d180401725353b56eb04b1d
Keiho Sakapon <git@saka-pon.net>
2024-11-14 00:02:05 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Int3/ABC120_D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Int3
+{
+	// Test: https://atcoder.jp/contests/abc120/tasks/abc120_d
+	class ABC120_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var r = new long[m];
+			var s = (long)n * (n - 1) / 2;
+			var uf = new UnionFind(n + 1);
+
+			for (int j = m - 1; j >= 0; j--)
+			{
+				r[j] = s;
+				var (a, b) = es[j];
+				var t = (long)uf.Find(a).Size * uf.Find(b).Size;
+				if (uf.Union(a, b)) s -= t;
+			}
+			return string.Join("\n", r);
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Int3/ABC229_E.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Int3
+{
+	// Test: https://atcoder.jp/contests/abc229/tasks/abc229_e
+	class ABC229_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int u, int v) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var map = es.ToLookup(e => e.u, e => e.v);
+
+			var r = new List<int>();
+			var uf = new UnionFind(n + 1);
+
+			for (int v = n; v > 0; v--)
+			{
+				r.Add(uf.GroupsCount - v - 1);
+
+				foreach (var nv in map[v])
+				{
+					uf.Union(v, nv);
+				}
+			}
+
+			r.Reverse();
+			return string.Join("\n", r);
+		}
+	}
+}
