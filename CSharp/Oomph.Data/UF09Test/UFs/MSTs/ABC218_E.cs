26230c8277aa4e6ca8b2e37dfe02b48dfb5b92c7
Keiho Sakapon <git@saka-pon.net>
2024-11-13 18:50:47 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/MSTs/ABC218_E.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.MSTs
+{
+	// Test: https://atcoder.jp/contests/abc218/tasks/abc218_e
+	class ABC218_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static (int, int, int c) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read3());
+
+			var r = 0L;
+			var uf = new UnionFind(n + 1);
+
+			foreach (var (u, v, c) in es.OrderBy(e => e.c))
+			{
+				if (!uf.Union(u, v) && c >= 0) r += c;
+			}
+			return r;
+		}
+	}
+}
