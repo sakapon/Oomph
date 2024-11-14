4dbf7a439f5a3f86ca308fb456b95e498d6cfd49
Keiho Sakapon <git@saka-pon.net>
2024-11-14 10:24:41 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Int2/ABC075_C.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Int2
+{
+	// Test: https://atcoder.jp/contests/abc075/tasks/abc075_c
+	class ABC075_C
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			return Enumerable.Range(0, m).Count(IsBridge);
+
+			bool IsBridge(int ei)
+			{
+				var uf = new UnionFind(n + 1);
+
+				for (int j = 0; j < m; j++)
+				{
+					if (j == ei) continue;
+
+					var (a, b) = es[j];
+					uf.Union(a, b);
+				}
+				return uf.GroupsCount != 2;
+			}
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Int2/ARC114_B.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Int2
+{
+	// Test: https://atcoder.jp/contests/arc114/tasks/arc114_b
+	class ARC114_B
+	{
+		const long M = 998244353;
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var f = Read();
+
+			var uf = new UnionFind(n + 1);
+			for (int i = 1; i <= n; i++)
+				uf.Union(i, f[i - 1]);
+
+			var r = 1L;
+			var c = uf.GroupsCount - 1;
+			while (c-- > 0)
+				r = r * 2 % M;
+			return (r - 1 + M) % M;
+		}
+	}
+}
