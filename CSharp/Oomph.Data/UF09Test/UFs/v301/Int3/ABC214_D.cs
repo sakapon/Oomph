fc86229a508257e5ddc374c596a68e6ce7d0f76f
Keiho Sakapon <git@saka-pon.net>
2024-11-14 10:56:02 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Int3/ABC214_D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Int3
+{
+	// Test: https://atcoder.jp/contests/abc214/tasks/abc214_d
+	class ABC214_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int, int w) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n - 1], _ => Read3());
+
+			var r = 0L;
+			var uf = new UnionFind(n + 1);
+
+			foreach (var (u, v, w) in es.OrderBy(e => e.w))
+			{
+				r += (long)w * uf.Find(u).Size * uf.Find(v).Size;
+				uf.Union(u, v);
+			}
+			return r;
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Int3/ARC027_B.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Int3
+{
+	// Test: https://atcoder.jp/contests/arc027/tasks/arc027_2
+	class ARC027_B
+	{
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var s = Console.ReadLine();
+			var t = Console.ReadLine();
+
+			var st = s + t;
+			var rn = Enumerable.Range(0, n).ToArray();
+
+			// 数字が確定しているインデックス
+			var u = rn.Select(i => char.IsNumber(s[i]) || char.IsNumber(t[i])).ToArray();
+
+			var uf = new UnionFind(n);
+			foreach (var g in Enumerable.Range(0, 2 * n).GroupBy(i => st[i]))
+			{
+				if (char.IsNumber(g.Key)) continue;
+
+				var a = g.Select(i => i < n ? i : i - n).ToArray();
+				for (int j = 1; j < a.Length; j++)
+					uf.Union(a[j - 1], a[j]);
+			}
+
+			var r = 1L;
+			foreach (var g in uf.ToGroups())
+			{
+				if (g.Any(i => u[i])) continue;
+				r *= g.Contains(0) ? 9 : 10;
+			}
+			return r;
+		}
+	}
+}
