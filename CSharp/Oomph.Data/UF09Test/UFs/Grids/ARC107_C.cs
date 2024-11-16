7d3306d64ff79e05a934aa39f3355a35cada44e9
Keiho Sakapon <git@saka-pon.net>
2024-11-16 21:09:32 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/Grids/ARC107_C.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.Grids
+{
+	// Test: https://atcoder.jp/contests/arc107/tasks/arc107_c
+	class ARC107_C
+	{
+		const long M = 998244353;
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, k) = Read2();
+			var a = Array.ConvertAll(new bool[n], _ => Read());
+
+			var rn = Enumerable.Range(0, n).ToArray();
+
+			var r = SolveColumn(a);
+			a = Array.ConvertAll(rn, j => Array.ConvertAll(rn, i => a[i][j]));
+			r *= SolveColumn(a);
+			return r % M;
+
+			long SolveColumn(int[][] a)
+			{
+				var uf = new UnionFind(n);
+
+				for (int x = 0; x < n; x++)
+				{
+					for (int y = x + 1; y < n; y++)
+					{
+						if (Array.TrueForAll(rn, i => a[i][x] + a[i][y] <= k))
+						{
+							uf.Union(x, y);
+						}
+					}
+				}
+
+				var r = 1L;
+				foreach (var g in uf.GetGroupInfoes())
+				{
+					var c = g.Size;
+					for (int i = 1; i <= c; i++)
+					{
+						r *= i;
+						r %= M;
+					}
+				}
+				return r;
+			}
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Int4/ABC328_E.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Int4
+{
+	// Test: https://atcoder.jp/contests/abc328/tasks/abc328_e
+	class ABC328_E
+	{
+		static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+		static (int, int, long) Read3L() { var a = ReadL(); return ((int)a[0], (int)a[1], a[2]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m, k) = Read3L();
+			var es = Array.ConvertAll(new bool[m], _ => Read3L());
+
+			var r = 1L << 60;
+
+			Combination(es, n - 1, p =>
+			{
+				var uf = new UnionFind(n + 1);
+				var s = 0L;
+
+				foreach (var (u, v, w) in p)
+				{
+					uf.Union(u, v);
+					s += w;
+				}
+
+				s %= k;
+				if (uf.GroupsCount == 2 && r > s) r = s;
+				return false;
+			});
+
+			return r;
+		}
+
+		public static void Combination<T>(T[] a, int r, Func<T[], bool> action)
+		{
+			var n = a.Length;
+			var p = new T[r];
+			DFS(0, 0);
+
+			bool DFS(int v, int si)
+			{
+				if (v == r) return action(p);
+
+				for (int i = si; r - v <= n - i; ++i)
+				{
+					p[v] = a[i];
+					if (DFS(v + 1, i + 1)) return true;
+				}
+				return false;
+			}
+		}
+	}
+}
