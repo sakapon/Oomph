3a01660dd020896d6b9b885633f31180327b2238
Keiho Sakapon <git@saka-pon.net>
2024-11-18 12:26:34 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/Grids/ABC219_E.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+// fake vertexes
+namespace UF09Test.UFs.Grids
+{
+	// Test: https://atcoder.jp/contests/abc219/tasks/abc219_e
+	class ABC219_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			const int h = 4;
+			const int w = 4;
+			var a0 = Array.ConvertAll(new bool[h], _ => Read());
+
+			const int n = h * w;
+			var p = a0.SelectMany(t => t.Select(x => x != 0)).ToArray();
+
+			var r = 0;
+			var f = new bool[n];
+
+			for (int x = 0; x < 1 << n; x++)
+			{
+				for (int v = 0; v < n; v++)
+					f[v] = (x & (1 << v)) != 0;
+
+				var ok = true;
+				for (int v = 0; v < n; v++)
+				{
+					if (p[v] && !f[v])
+					{
+						ok = false;
+						break;
+					}
+				}
+				if (!ok) continue;
+
+				var uf = new UnionFind(n + 1);
+
+				for (int i = 0; i < h; i++)
+					for (int j = 0; j < w; j++)
+					{
+						var v = w * i + j;
+
+						if (j > 0)
+						{
+							if (f[v] == f[v - 1]) uf.Union(v, v - 1);
+						}
+
+						if (i > 0)
+						{
+							if (f[v] == f[v - w]) uf.Union(v, v - w);
+						}
+
+						// 外周
+						if (i == 0 || j == 0 || i == h - 1 || j == w - 1)
+						{
+							if (!f[v]) uf.Union(v, n);
+						}
+					}
+
+				if (uf.GroupsCount == 2) r++;
+			}
+			return r;
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/MSTs/ARC026_D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.MSTs
+{
+	// Test: https://atcoder.jp/contests/arc026/tasks/arc026_4
+	class ARC026_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static (int, int, int c, int t) Read4() { var a = Read(); return (a[0], a[1], a[2], a[3]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read4());
+
+			return First(0, 1 << 30, x =>
+			{
+				var s = 0.0;
+				var uf = new UnionFind(n + 1);
+
+				foreach (var (u, v, c, t) in es.OrderBy(p => p.c - p.t * x))
+				{
+					var d = c - t * x;
+					if (d <= 0 || !uf.AreSame(u, v))
+					{
+						s += d;
+						uf.Union(u, v);
+					}
+				}
+				return s <= 0;
+			}, 2);
+		}
+
+		static double First(double l, double r, Func<double, bool> f, int digits = 9)
+		{
+			double m;
+			while (Math.Round(r - l, digits) > 0) if (f(m = l + (r - l) / 2)) r = m; else l = m;
+			return r;
+		}
+	}
+}
