f01a827f18fa708d8eec63247c782fd6f4eb7c09
Keiho Sakapon <git@saka-pon.net>
2024-11-21 22:40:15 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Int3/ABC277_D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Int3
+{
+	// Test: https://atcoder.jp/contests/abc277/tasks/abc277_d
+	class ABC277_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var a = ReadL();
+
+			Array.Sort(a);
+			var s = a.Sum();
+			var uf = new UnionFind(n);
+
+			for (int i = 1; i < n; i++)
+			{
+				var d = a[i] - a[i - 1];
+				if (d == 0 || d == 1)
+				{
+					uf.Union(i, i - 1);
+				}
+			}
+
+			if (a[0] == 0 && a[^1] == m - 1)
+			{
+				uf.Union(0, n - 1);
+			}
+
+			return s - uf.ToGroups().Max(g => g.Sum(i => a[i]));
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Int4/ABC335_E.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Int4
+{
+	// Test: https://atcoder.jp/contests/abc335/tasks/abc335_e
+	class ABC335_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var a = Read().Prepend(0).ToArray();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var uf = new UnionFind(n + 1);
+			foreach (var (u, v) in es)
+			{
+				if (a[u] == a[v])
+					uf.Union(u, v);
+			}
+
+			var map = Array.ConvertAll(new bool[n + 1], _ => new List<int>());
+			var indeg = new int[n + 1];
+
+			foreach (var e in es)
+			{
+				var (u, v) = e;
+
+				u = uf.Find(u).Key;
+				v = uf.Find(v).Key;
+				if (u == v) continue;
+
+				if (a[u] > a[v]) (u, v) = (v, u);
+				map[u].Add(v);
+				indeg[v]++;
+			}
+
+			var dp = new int[n + 1];
+			dp[uf.Find(1).Key] = 1;
+
+			var q = new Queue<int>();
+			for (int v = 1; v <= n; v++)
+			{
+				if (indeg[v] == 0)
+					q.Enqueue(v);
+			}
+
+			while (q.TryDequeue(out var v))
+			{
+				foreach (var nv in map[v])
+				{
+					if (dp[v] > 0)
+						Chmax(ref dp[nv], dp[v] + 1);
+
+					if (--indeg[nv] == 0)
+						q.Enqueue(nv);
+				}
+			}
+
+			return dp[uf.Find(n).Key];
+		}
+
+		public static int Chmax(ref int x, int v) => x < v ? x = v : x;
+	}
+}
