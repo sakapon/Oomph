d5b0e333be5fbd4806d908713b7fcd3a83477f8e
sakapon <git@saka-pon.net>
2024-11-21 14:16:10 +0900

    Add UnionFind 123

---
+++ Python/oomph-data-ws/uf_309_lib/ufs/int_beta/unionfind_123.py
@@ -0,0 +1,37 @@
+class UnionFind:
+    """
+    Represents a disjoint-set data structure, using union by size and path compression.
+
+    - parents[root] = -size
+    - parents[non-root] = parent
+
+    time complexity: O(α(n))
+    """
+
+    def __init__(self, n: int):
+        self.parents = [-1] * n
+
+    def find(self, x: int):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def are_same(self, x: int, y: int):
+        return self.find(x) == self.find(y)
+
+    def get_group_size(self, x: int):
+        return -self.parents[self.find(x)]
+
+    def union(self, x: int, y: int):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return False
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+        return True
---
+++ Python/oomph-data-ws/uf_309_test/ufs/int_beta/ATC001_B.py
@@ -0,0 +1,19 @@
+# https://atcoder.jp/contests/atc001/tasks/unionfind_a
+# https://atcoder.jp/contests/practice2/tasks/practice2_a
+
+from ....uf_309_lib.ufs.int_beta.unionfind_103 import UnionFind
+
+n, qc = map(int, input().split())
+
+r = []
+uf = UnionFind(n)
+
+for qi in range(qc):
+    t, u, v = map(int, input().split())
+    if t == 0:
+        uf.union(u, v)
+    else:
+        r.append("Yes" if uf.are_same(u, v) else "No")
+        # r.append("1" if uf.are_same(u, v) else "0")
+
+print("\n".join(r))
