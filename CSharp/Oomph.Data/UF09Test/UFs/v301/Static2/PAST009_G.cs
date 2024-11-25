9153e9cf13453f47e377a7a768cd77b55d62029d
Keiho Sakapon <git@saka-pon.net>
2024-11-25 23:52:11 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/Static2/PAST009_G.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.Static2
+{
+	// Test: https://atcoder.jp/contests/past202112-open/tasks/past202112_g
+	class PAST009_G
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, qc) = Read2();
+			var qs = Array.ConvertAll(new bool[qc], _ => Read3());
+
+			var r = new List<bool>();
+			var map = new bool[n + 1, n + 1];
+
+			foreach (var q in qs)
+			{
+				var (t, u, v) = q;
+				if (t == 1)
+				{
+					if (u > v) (u, v) = (v, u);
+					map[u, v] ^= true;
+				}
+				else
+				{
+					r.Add(Check(u, v));
+				}
+			}
+			return string.Join("\n", r.Select(b => b ? "Yes" : "No"));
+
+			bool Check(int s, int t)
+			{
+				var uf = new UnionFind(n + 1);
+				for (int u = 1; u <= n; u++)
+					for (int v = u + 1; v <= n; v++)
+						if (map[u, v])
+							uf.Union(u, v);
+				return uf.AreSame(s, t);
+			}
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/v311/ABC228_D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v311;
+
+namespace UF09Test.UFs.v311
+{
+	// Test: https://atcoder.jp/contests/abc228/tasks/abc228_d
+	class ABC228_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+		static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var qc = int.Parse(Console.ReadLine());
+			var qs = Array.ConvertAll(new bool[qc], _ => Read2L());
+
+			const int n = 1 << 20;
+			var a = new long[n];
+			Array.Fill(a, -1);
+			var rn = Enumerable.Range(0, n).ToArray();
+
+			var r = new List<long>();
+			// a[i] == -1 である最も近い右のインデックス i
+			var uf = new UnionFind<int>(n, (x, y) => y, true, rn);
+
+			foreach (var (t, x) in qs)
+			{
+				if (t == 1)
+				{
+					var h = uf.Find((int)(x % n)).Value;
+					a[h] = x;
+					uf.Union(h, (h + 1) % n);
+				}
+				else
+				{
+					r.Add(a[x % n]);
+				}
+			}
+			return string.Join("\n", r);
+		}
+	}
+}
