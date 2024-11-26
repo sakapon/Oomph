84624cc75a7b38fce2266f0c01a9f9e0b6382074
Keiho Sakapon <git@saka-pon.net>
2024-11-26 09:26:22 +0900

    Copy class

---
+++ CSharp/Oomph.Data/UF09Lib/UFs/TypedBeta/UnionFind.411.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+// int vertexes, data augmentation
+
+namespace Oomph.Data.UF09Lib.UFs.v411
+{
+	[System.Diagnostics.DebuggerDisplay(@"ItemsCount = {ItemsCount}, GroupsCount = {GroupsCount}")]
+	public class UnionFind<TValue>
+	{
+		public class Node
+		{
+			public int Key { get; internal set; }
+			internal Node Parent;
+			public int Size { get; internal set; } = 1;
+			public TValue Value { get; set; }
+			public override string ToString() => Parent == null ? $"{Key}, Size = {Size}, Value = {Value}" : $"{Key} (not root)";
+		}
+
+		readonly Node[] nodes;
+		public int ItemsCount => nodes.Length;
+		public int GroupsCount { get; private set; }
+		public Func<TValue, TValue, TValue> MergeValues { get; }
+		public bool KeepOrder { get; }
+
+		// (parent root, child root)
+		public event Action<int, int> United;
+
+		public UnionFind(int n, Func<TValue, TValue, TValue> mergeValues, bool keepOrder, TValue[] values = null)
+		{
+			values ??= new TValue[n];
+			nodes = new Node[n];
+			for (int i = 0; i < n; ++i) nodes[i] = new Node { Key = i, Value = values[i] };
+			GroupsCount = n;
+			MergeValues = mergeValues;
+			KeepOrder = keepOrder;
+		}
+
+		Node Find(Node n) => n.Parent == null ? n : n.Parent = Find(n.Parent);
+		public Node Find(int x) => Find(nodes[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+
+		public bool Union(int x, int y)
+		{
+			var nx = Find(x);
+			var ny = Find(y);
+			if (nx == ny) return false;
+
+			TValue v = default;
+			// 左右の順序を保って値をマージします。
+			if (KeepOrder) v = MergeValues(nx.Value, ny.Value);
+
+			if (nx.Size < ny.Size) (nx, ny) = (ny, nx);
+			ny.Parent = nx;
+			nx.Size += ny.Size;
+			--GroupsCount;
+			// 親子の順序で値をマージします。
+			if (!KeepOrder) v = MergeValues(nx.Value, ny.Value);
+			nx.Value = v;
+			United?.Invoke(nx.Key, ny.Key);
+			return true;
+		}
+
+		// 根とサイズと値の情報のみを取得します。
+		public Node[] GetGroupInfoes() => Array.FindAll(nodes, n => n.Parent == null);
+		public ILookup<Node, int> ToGroups() => nodes.ToLookup(Find, n => n.Key);
+	}
+}
