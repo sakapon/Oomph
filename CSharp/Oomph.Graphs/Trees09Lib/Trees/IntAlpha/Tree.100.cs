9000fc521557fdf258738992a16d78337f7e17f2
Keiho Sakapon <git@saka-pon.net>
2025-05-05 00:01:31 +0900

    Add Tree class

---
+++ CSharp/Oomph.Graphs/Trees09Lib/Trees/IntAlpha/Tree.100.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+// int vertexes, array-based
+
+namespace Oomph.Graphs.Trees09Lib.Trees.v100
+{
+	public class Tree
+	{
+		public int Count { get; }
+		public int Root { get; }
+		public List<int>[] Map { get; }
+		public int[] Depths { get; }
+		public int[] Parents { get; }
+
+		public Tree(int n, int root, List<int>[] map)
+		{
+			Count = n;
+			Root = root;
+			Map = map;
+			Depths = Array.ConvertAll(Map, _ => -1);
+			Parents = Array.ConvertAll(Map, _ => -1);
+
+			Depths[root] = 0;
+			DFS(root);
+
+			void DFS(int v)
+			{
+				foreach (var nv in Map[v])
+				{
+					if (nv == Parents[v]) continue;
+					Depths[nv] = Depths[v] + 1;
+					Parents[nv] = v;
+					DFS(nv);
+				}
+			}
+		}
+	}
+}
---
+++ CSharp/Oomph.Graphs/Trees09Test/Trees/v300/T90_026.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Graphs.Trees09Lib.Trees.v100;
+
+namespace Trees09Test.Trees.v300
+{
+	// Test: https://atcoder.jp/contests/typical90/tasks/typical90_z
+	class T90_026
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n - 1], _ => Read2());
+
+			var map = ToMapList(n + 1, es, true);
+			var tree = new Tree(n + 1, 1, map);
+
+			var rn = Enumerable.Range(1, n).ToArray();
+			var r0 = Array.FindAll(rn, v => tree.Depths[v] % 2 == 0);
+			var r1 = Array.FindAll(rn, v => tree.Depths[v] % 2 == 1);
+			var r = r0.Length >= r1.Length ? r0 : r1;
+			return string.Join(" ", r[..(n / 2)]);
+		}
+
+		public static List<int>[] ToMapList(int n, (int u, int v)[] es, bool twoway)
+		{
+			var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+			foreach (var (u, v) in es)
+			{
+				map[u].Add(v);
+				if (twoway) map[v].Add(u);
+			}
+			return map;
+		}
+	}
+}
