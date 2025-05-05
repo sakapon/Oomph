2e521219d495aaf33db672d846942e5a0280ee3e
Keiho Sakapon <git@saka-pon.net>
2025-05-05 23:42:26 +0900

    Add Tree class

---
+++ CSharp/Oomph.Graphs/Trees09Lib/Trees/IntBeta/Tree.200.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+// int vertexes, array-based
+
+namespace Oomph.Graphs.Trees09Lib.Trees.v200
+{
+	public class Tree
+	{
+		public static List<int>[] ToListMap(int n, (int u, int v)[] edges, bool twoway)
+		{
+			var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+			foreach (var (u, v) in edges)
+			{
+				map[u].Add(v);
+				if (twoway) map[v].Add(u);
+			}
+			return map;
+		}
+
+		public int Count { get; }
+		public List<int>[] Map { get; }
+		public int Root { get; }
+		public int[] Parents { get; }
+		public int[] Depths { get; }
+
+		// この Euler Tour では方向を記録しません。
+		// order -> vertex
+		public int[] Tour { get; }
+		// vertex -> orders
+		public List<int>[] TourMap { get; }
+		// depth -> orders
+		public List<int>[] DepthToTourMap { get; }
+
+		public Tree(int n, (int u, int v)[] edges, int root)
+		{
+			Count = n;
+			Map = ToListMap(n, edges, true);
+			Root = root;
+			Parents = Array.ConvertAll(Map, _ => -1);
+			Depths = Array.ConvertAll(Map, _ => -1);
+
+			var tour = new List<int>();
+			TourMap = Array.ConvertAll(Map, _ => new List<int>());
+			DepthToTourMap = Array.ConvertAll(Map, _ => new List<int>());
+
+			Depths[root] = 0;
+			DFS(root);
+
+			Tour = tour.ToArray();
+
+			void DFS(int v)
+			{
+				TourMap[v].Add(tour.Count);
+				DepthToTourMap[Depths[v]].Add(tour.Count);
+				tour.Add(v);
+
+				foreach (var nv in Map[v])
+				{
+					if (nv == Parents[v]) continue;
+					Parents[nv] = v;
+					Depths[nv] = Depths[v] + 1;
+					DFS(nv);
+
+					TourMap[v].Add(tour.Count);
+					DepthToTourMap[Depths[v]].Add(tour.Count);
+					tour.Add(v);
+				}
+			}
+		}
+	}
+}
---
+++ CSharp/Oomph.Graphs/Trees09Test/Trees/v300/LCA_ABC014_D.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Graphs.Trees09Lib.Trees.v200;
+
+namespace Trees09Test.Trees.v300
+{
+	// Test: https://atcoder.jp/contests/abc014/tasks/abc014_4
+	class LCA_ABC014_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n - 1], _ => Read2());
+			var qc = int.Parse(Console.ReadLine());
+			var qs = Array.ConvertAll(new bool[qc], _ => Read2());
+
+			var tree = new Tree(n + 1, es, 1);
+
+			return string.Join("\n", qs.Select(q =>
+			{
+				var (a, b) = q;
+				if (tree.TourMap[b][0] < tree.TourMap[a][0]) (a, b) = (b, a);
+				if (tree.TourMap[b][^1] <= tree.TourMap[a][^1]) return tree.Depths[b] - tree.Depths[a] + 1;
+
+				var (so, eo) = (tree.TourMap[a][^1], tree.TourMap[b][0]);
+
+				var d = First(0, Math.Min(tree.Depths[a], tree.Depths[b]), dx =>
+				{
+					var l = tree.DepthToTourMap[dx];
+					var si = First(0, l.Count, x => l[x] >= so);
+					var ei = First(0, l.Count, x => l[x] > eo);
+					return si < ei;
+				});
+
+				return tree.Depths[a] + tree.Depths[b] - 2 * d + 1;
+			}));
+		}
+
+		static int First(int l, int r, Func<int, bool> f)
+		{
+			int m;
+			while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+			return r;
+		}
+	}
+}
