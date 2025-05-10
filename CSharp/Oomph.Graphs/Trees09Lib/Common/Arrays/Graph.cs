aa49c5d61a32bdc7f03aa4a2ab59d3f4afe21886
Keiho Sakapon <git@saka-pon.net>
2025-05-10 12:05:13 +0900

    Add classes

---
+++ CSharp/Oomph.Graphs/Trees09Lib/Common/Arrays/Graph.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oomph.Graphs.Trees09Lib.Common.Arrays
+{
+	public class UnweightedGraph
+	{
+		public int Count => Map.Length;
+		public List<int>[] Map { get; }
+
+		public UnweightedGraph(int n) => Map = Array.ConvertAll(new bool[n], _ => new List<int>());
+		public UnweightedGraph(int n, (int u, int v)[] edges, bool twoway) : this(n)
+		{
+			foreach (var (u, v) in edges) AddEdge(u, v, twoway);
+		}
+
+		public void AddEdge(int u, int v, bool twoway)
+		{
+			Map[u].Add(v);
+			if (twoway) Map[v].Add(u);
+		}
+	}
+
+	public class WeightedGraph
+	{
+		public int Count => Map.Length;
+		public List<(int, int)>[] Map { get; }
+
+		public WeightedGraph(int n) => Map = Array.ConvertAll(new bool[n], _ => new List<(int, int)>());
+		public WeightedGraph(int n, (int u, int v, int w)[] edges, bool twoway) : this(n)
+		{
+			foreach (var (u, v, w) in edges) AddEdge(u, v, w, twoway);
+		}
+
+		public void AddEdge(int u, int v, int w, bool twoway)
+		{
+			Map[u].Add((v, w));
+			if (twoway) Map[v].Add((u, w));
+		}
+	}
+}
---
+++ CSharp/Oomph.Graphs/Trees09Lib/Common/Arrays/GraphHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oomph.Graphs.Trees09Lib.Common.Arrays
+{
+	public static class UnweightedGraphHelper
+	{
+		public static int[][] ToMap(int n, (int u, int v)[] edges, bool twoway) => Array.ConvertAll(ToListMap(n, edges, twoway), l => l.ToArray());
+		public static List<int>[] ToListMap(int n, (int u, int v)[] edges, bool twoway)
+		{
+			var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+			foreach (var (u, v) in edges)
+			{
+				map[u].Add(v);
+				if (twoway) map[v].Add(u);
+			}
+			return map;
+		}
+	}
+
+	public static class WeightedGraphHelper
+	{
+		public static (int, int)[][] ToMap(int n, (int u, int v, int w)[] edges, bool twoway) => Array.ConvertAll(ToListMap(n, edges, twoway), l => l.ToArray());
+		public static List<(int, int)>[] ToListMap(int n, (int u, int v, int w)[] edges, bool twoway)
+		{
+			var map = Array.ConvertAll(new bool[n], _ => new List<(int, int)>());
+			foreach (var (u, v, w) in edges)
+			{
+				map[u].Add((v, w));
+				if (twoway) map[v].Add((u, w));
+			}
+			return map;
+		}
+	}
+}
