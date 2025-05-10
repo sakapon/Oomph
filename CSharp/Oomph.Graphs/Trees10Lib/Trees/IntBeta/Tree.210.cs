048b621e44c5be7087630790ba401b8161f2b274
Keiho Sakapon <git@saka-pon.net>
2025-05-11 01:59:49 +0900

    Add methods

--- CSharp/Oomph.Graphs/Trees10Lib/Trees/IntBeta/Tree.210.cs
+++ CSharp/Oomph.Graphs/Trees10Lib/Trees/IntBeta/Tree.210.cs
@@ -12,6 +12,14 @@ namespace Oomph.Graphs.Trees10Lib.Trees.v210
 		public int[] Parents { get; }
 		public int[] Depths { get; }
 
+		// この Euler Tour では方向を記録しません。
+		// order -> vertex
+		public int[] Tour { get; private set; }
+		// vertex -> orders
+		public List<int>[] TourMap { get; private set; }
+		// depth -> orders
+		public List<int>[] DepthToTourMap { get; private set; }
+
 		public Tree(UnweightedGraph g, int root)
 		{
 			Map = g.Map;
@@ -22,26 +30,68 @@ namespace Oomph.Graphs.Trees10Lib.Trees.v210
 
 		void Reroot(int root)
 		{
+			var tour = new List<int>();
+			TourMap = Array.ConvertAll(Map, _ => new List<int>());
+			DepthToTourMap = Array.ConvertAll(Map, _ => new List<int>());
+
 			Root = root;
 			Parents[root] = -1;
 			Depths[root] = 0;
 			DFS(root);
-		}
 
-		void DFS(int v)
-		{
-			foreach (var nv in Map[v])
+			Tour = tour.ToArray();
+
+			void DFS(int v)
 			{
-				if (nv == Parents[v]) continue;
-				Parents[nv] = v;
-				Depths[nv] = Depths[v] + 1;
-				DFS(nv);
+				TourMap[v].Add(tour.Count);
+				DepthToTourMap[Depths[v]].Add(tour.Count);
+				tour.Add(v);
+
+				foreach (var nv in Map[v])
+				{
+					if (nv == Parents[v]) continue;
+					Parents[nv] = v;
+					Depths[nv] = Depths[v] + 1;
+					DFS(nv);
+
+					TourMap[v].Add(tour.Count);
+					DepthToTourMap[Depths[v]].Add(tour.Count);
+					tour.Add(v);
+				}
 			}
 		}
 
 		public IEnumerable<int> GetPath(int v)
 		{
 			for (; v != -1; v = Parents[v]) yield return v;
+		}
+
+		public bool IsAncestor(int a, int b)
+		{
+			return TourMap[a][0] <= TourMap[b][0] && TourMap[b][^1] <= TourMap[a][^1];
+		}
+
+		public int GetLcaDepth(int a, int b)
+		{
+			if (TourMap[b][0] < TourMap[a][0]) (a, b) = (b, a);
+			if (TourMap[b][^1] <= TourMap[a][^1]) return Depths[a];
+
+			var (so, eo) = (TourMap[a][^1], TourMap[b][0]);
+
+			return First(0, Math.Min(Depths[a], Depths[b]), dx =>
+			{
+				var l = DepthToTourMap[dx];
+				var si = First(0, l.Count, x => l[x] >= so);
+				var ei = First(0, l.Count, x => l[x] > eo);
+				return si < ei;
+			});
+		}
+
+		static int First(int l, int r, Func<int, bool> f)
+		{
+			int m;
+			while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+			return r;
 		}
 	}
 }
---
+++ CSharp/Oomph.Graphs/Trees10Test/Trees/v300/PAST001_K.cs
@@ -0,0 +1,38 @@
+﻿using Oomph.Graphs.Trees10Lib.Common.Arrays;
+using Oomph.Graphs.Trees10Lib.Trees.v210;
+
+namespace Trees10Test.Trees.v300
+{
+	// Test: https://atcoder.jp/contests/past201912-open/tasks/past201912_k
+	class PAST001_K
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var p = Array.ConvertAll(new bool[n], _ => int.Parse(Console.ReadLine()));
+			var qc = int.Parse(Console.ReadLine());
+			var qs = Array.ConvertAll(new bool[qc], _ => Read2());
+
+			var g = new UnweightedGraph(n + 1);
+			var root = 0;
+
+			for (int v = 1; v <= n; v++)
+			{
+				if (p[v - 1] == -1)
+					root = v;
+				else
+					g.AddEdge(p[v - 1], v, false);
+			}
+
+			var tree = new Tree(g, root);
+			return string.Join("\n", qs.Select(q =>
+			{
+				var (a, b) = q;
+				return tree.IsAncestor(b, a) ? "Yes" : "No";
+			}));
+		}
+	}
+}
