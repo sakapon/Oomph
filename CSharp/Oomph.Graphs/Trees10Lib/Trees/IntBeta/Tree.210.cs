fc059de252506c7b3073da3c555e06b5c71a42e7
Keiho Sakapon <git@saka-pon.net>
2025-05-10 23:42:37 +0900

    Add classes

---
+++ CSharp/Oomph.Graphs/Trees10Lib/Trees/IntBeta/Tree.210.cs
@@ -0,0 +1,42 @@
+﻿using Oomph.Graphs.Trees10Lib.Common.Arrays;
+
+// int vertexes, array-based
+
+namespace Oomph.Graphs.Trees10Lib.Trees.v210
+{
+	public class Tree
+	{
+		public int Count => Map.Length;
+		public List<int>[] Map { get; }
+		public int Root { get; private set; }
+		public int[] Parents { get; }
+		public int[] Depths { get; }
+
+		public Tree(UnweightedGraph g, int root)
+		{
+			Map = g.Map;
+			Parents = Array.ConvertAll(Map, _ => -1);
+			Depths = Array.ConvertAll(Map, _ => -1);
+			Reroot(root);
+		}
+
+		void Reroot(int root)
+		{
+			Root = root;
+			Parents[root] = -1;
+			Depths[root] = 0;
+			DFS(root);
+		}
+
+		void DFS(int v)
+		{
+			foreach (var nv in Map[v])
+			{
+				if (nv == Parents[v]) continue;
+				Parents[nv] = v;
+				Depths[nv] = Depths[v] + 1;
+				DFS(nv);
+			}
+		}
+	}
+}
