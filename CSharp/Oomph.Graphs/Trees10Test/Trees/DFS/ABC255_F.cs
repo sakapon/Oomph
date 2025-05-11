fa13c7811cb0c8576fa6be5007e5191f3f9e9b4f
Keiho Sakapon <git@saka-pon.net>
2025-05-11 13:23:52 +0900

    Add test

---
+++ CSharp/Oomph.Graphs/Trees10Test/Trees/DFS/ABC255_F.cs
@@ -0,0 +1,48 @@
+﻿namespace Trees10Test.Trees.DFS
+{
+	// Test: https://atcoder.jp/contests/abc255/tasks/abc255_f
+	class ABC255_F
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var P = Read();
+			var I = Read();
+
+			if (P[0] != 1) return -1;
+
+			var pq = new Queue<int>(P);
+			var iq = new Queue<int>(I);
+			var u = new bool[n + 1];
+			var r = Array.ConvertAll(new bool[n + 1], _ => new int[2]);
+
+			var ok = DFS(-1, -1);
+			if (!ok) return -1;
+			return string.Join("\n", r[1..].Select(a => $"{a[0]} {a[1]}"));
+
+			bool DFS(int pv, int index)
+			{
+				var v = pq.Dequeue();
+				u[v] = true;
+				if (pv != -1) r[pv][index] = v;
+				if (iq.Peek() != v && !DFS(v, 0)) return false;
+				if (iq.Dequeue() != v) return false;
+				if (iq.Count > 0 && !u[iq.Peek()] && !DFS(v, 1)) return false;
+				return true;
+			}
+		}
+
+		public static List<int>[] ToListMap(int n, (int u, int v)[] edges, bool twoway)
+		{
+			var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+			foreach (var (u, v) in edges)
+			{
+				map[u].Add(v);
+				if (twoway) map[v].Add(u);
+			}
+			return map;
+		}
+	}
+}
---
+++ CSharp/Oomph.Graphs/Trees10Test/Trees/v300/ABC209_D.cs
@@ -0,0 +1,27 @@
+﻿using Oomph.Graphs.Trees10Lib.Common.Arrays;
+using Oomph.Graphs.Trees10Lib.Trees.v210;
+
+namespace Trees10Test.Trees.v300
+{
+	// Test: https://atcoder.jp/contests/abc209/tasks/abc209_d
+	class ABC209_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, qc) = Read2();
+			var es = Array.ConvertAll(new bool[n - 1], _ => Read2());
+			var qs = Array.ConvertAll(new bool[qc], _ => Read2());
+
+			var g = new UnweightedGraph(n + 1, es, true);
+			var tree = new Tree(g, 1);
+			return string.Join("\n", qs.Select(q =>
+			{
+				var (a, b) = q;
+				return (tree.Depths[a] - tree.Depths[b]) % 2 == 0 ? "Town" : "Road";
+			}));
+		}
+	}
+}
