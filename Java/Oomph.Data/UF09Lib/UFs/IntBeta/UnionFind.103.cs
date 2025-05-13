6c2115156427f68b7404dfaac6b915bc90b863da
Keiho Sakapon <git@saka-pon.net>
2025-05-14 00:01:09 +0900

    Add UnionFind class

---
+++ Java/Oomph.Data/UF09Lib/UFs/IntBeta/UnionFind.103.cs
@@ -0,0 +1,43 @@
+﻿// path compression, union by size
+// O(α(n))
+
+public static class UnionFind
+{
+	int[] parents, sizes;
+
+	public UnionFind(int n)
+	{
+		parents = new int[n];
+		Arrays.fill(parents, -1);
+		sizes = new int[n];
+		Arrays.fill(sizes, 1);
+	}
+
+	public int Find(int x)
+	{
+		return parents[x] == -1 ? x : (parents[x] = Find(parents[x]));
+	}
+	public boolean AreSame(int x, int y)
+	{
+		return Find(x) == Find(y);
+	}
+	public int GetGroupSize(int x)
+	{
+		return sizes[Find(x)];
+	}
+
+	public boolean Union(int x, int y)
+	{
+		if ((x = Find(x)) == (y = Find(y))) return false;
+
+		if (sizes[x] < sizes[y])
+		{
+			int t = x;
+			x = y;
+			y = t;
+		}
+		parents[y] = x;
+		sizes[x] += sizes[y];
+		return true;
+	}
+}
---
+++ Java/Oomph.Data/UF09Test/UFs/v301/L1/ATC001_B.cs
@@ -0,0 +1,26 @@
+﻿import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int qc = sc.nextInt();
+
+		UnionFind uf = new UnionFind(n + 1);
+		while (qc-- > 0)
+		{
+			int t = sc.nextInt();
+			int u = sc.nextInt();
+			int v = sc.nextInt();
+
+			if (t == 0)
+			{
+				uf.Union(u, v);
+			}
+			else
+			{
+				System.out.println(uf.AreSame(u, v) ? "Yes" : "No");
+			}
+		}
+	}
+}
