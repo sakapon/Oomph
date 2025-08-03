71668be78046124296044d225e1dbc01323c2450
Keiho Sakapon <saka_pon@live.jp>
2025-08-03 18:38:53 +0900

    Add classes

---
+++ CSharp/Oomph.Data/Collections10Lib/HashTables/Direct/DirectTable.101.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Oomph.Data.Collections10Lib.HashTables.Direct.v101
+{
+	// Add, Contains, Remove
+	// Count, Clear
+	public class DirectSet
+	{
+		readonly bool[] b;
+		public int Count { get; private set; }
+
+		public DirectSet(int size)
+		{
+			b = new bool[size];
+		}
+
+		public void Clear()
+		{
+			Array.Clear(b);
+			Count = 0;
+		}
+
+		public bool Contains(int item)
+		{
+			return b[item];
+		}
+
+		public bool Add(int item)
+		{
+			if (b[item]) return false;
+			b[item] = true;
+			++Count;
+			return true;
+		}
+
+		public bool Remove(int item)
+		{
+			if (!b[item]) return false;
+			b[item] = false;
+			--Count;
+			return true;
+		}
+	}
+}
