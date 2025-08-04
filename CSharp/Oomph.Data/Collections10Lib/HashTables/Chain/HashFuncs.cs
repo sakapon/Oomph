b7a1f8b16fd5c611c3e53711bb84ce189d995324
sakapon <git@saka-pon.net>
2025-08-04 12:42:18 +0900

    Update code

--- CSharp/Oomph.Data/Collections10Lib/HashTables/Chain/HashFuncs.cs
+++ CSharp/Oomph.Data/Collections10Lib/HashTables/Chain/HashFuncs.cs
@@ -5,8 +5,8 @@ namespace Oomph.Data.Collections10Lib.HashTables.Chain
 	{
 		static readonly Random Random = new();
 
-		public static Func<uint, int, int> CreateMultiplication() => CreateMultiplication(Random.NextDouble());
-		public static Func<uint, int, int> CreateMultiplication(double a)
+		public static Func<uint, int, int> CreateMultiplicationDouble() => CreateMultiplicationDouble(Random.NextDouble());
+		public static Func<uint, int, int> CreateMultiplicationDouble(double a)
 		{
 			return (key, size) =>
 			{
@@ -17,13 +17,25 @@ namespace Oomph.Data.Collections10Lib.HashTables.Chain
 			};
 		}
 
+		public static Func<uint, int, int> CreateMultiplication() => CreateMultiplication((uint)Random.Next());
+		public static Func<uint, int, int> CreateMultiplication(uint a)
+		{
+			return (key, size) =>
+			{
+				var v = (ulong)key * a;
+				v &= uint.MaxValue;
+				v /= (1UL << 32) / (ulong)size;
+				return (int)v;
+			};
+		}
+
 		public static Func<uint, int, int> CreateUniversal() => CreateUniversal(Random.Next(), Random.Next());
 		public static Func<uint, int, int> CreateUniversal(int a, int b)
 		{
 			return (key, size) =>
 			{
 				var v = key * a + b;
-				v %= 4294968001;
+				v %= 4294967311;
 				v %= size;
 				return (int)v;
 			};
