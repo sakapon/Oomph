fffd3acdc893533ae982565c97aa8866951d126d
Keiho Sakapon <git@saka-pon.net>
2025-08-05 19:04:38 +0900

    Add test

---
+++ CSharp/Oomph.Data/Collections10Test/HashTables/Chain/ABC073_C.cs
@@ -0,0 +1,20 @@
+using Oomph.Data.Collections10Lib.HashTables.Chain.v101;
+
+namespace Collections10Test.HashTables.Chain
+{
+	// Test: https://atcoder.jp/contests/abc073/tasks/abc073_c
+	class ABC073_C
+	{
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var a = Array.ConvertAll(new bool[n], _ => int.Parse(Console.ReadLine()));
+
+			var set = new ChainHashSet<int>(18);
+			foreach (var x in a)
+				if (!set.Add(x)) set.Remove(x);
+			return set.Count;
+		}
+	}
+}
