8eea5974049a9781866a63482088b489a3518c4d
sakapon <git@saka-pon.net>
2025-08-05 14:01:13 +0900

    Add ChainHashMap class

--- CSharp/Oomph.Data/Collections10Lib/HashTables/Chain/ChainHashTable.101.cs
+++ CSharp/Oomph.Data/Collections10Lib/HashTables/Chain/ChainHashTable.101.cs
@@ -11,30 +11,31 @@ namespace Oomph.Data.Collections10Lib.HashTables.Chain.v101
 		}
 	}
 
-	// Add, Contains, Remove
+	// Add, ContainsKey, Remove, Item[]
 	// Count, Comparer, Clear
-	public class ChainHashSet<T>
+	public class ChainHashMap<TKey, TValue>
 	{
 		static int HashDefault(uint key, int size) => (int)((key * 2654435769) >> 32 - size);
 
 		public class Node
 		{
-			public T Item { get; init; }
+			public TKey Key { get; init; }
+			public TValue Value { get; set; }
 			internal Node Next;
 		}
 
 		readonly int bitSize;
 		readonly Node[] nodes;
 		public int Count { get; private set; }
-		public IEqualityComparer<T> Comparer { get; }
+		public IEqualityComparer<TKey> Comparer { get; }
 		readonly Func<uint, int, int> hashFunc;
-		int Hash(T key) => hashFunc((uint)(key?.GetHashCode() ?? 0), bitSize);
+		int Hash(TKey key) => hashFunc((uint)(key?.GetHashCode() ?? 0), bitSize);
 
-		public ChainHashSet(int bitSize, IEqualityComparer<T> comparer = null, Func<uint, int, int> hashFunc = null)
+		public ChainHashMap(int bitSize, IEqualityComparer<TKey> comparer = null, Func<uint, int, int> hashFunc = null)
 		{
 			this.bitSize = bitSize;
 			nodes = new Node[1 << bitSize];
-			Comparer = comparer ?? ComparerHelper.GetDefaultEquality<T>();
+			Comparer = comparer ?? ComparerHelper.GetDefaultEquality<TKey>();
 			this.hashFunc = hashFunc ?? HashDefault;
 		}
 
@@ -44,36 +45,36 @@ namespace Oomph.Data.Collections10Lib.HashTables.Chain.v101
 			Count = 0;
 		}
 
-		public bool Contains(T item)
+		public bool ContainsKey(TKey key)
 		{
-			var h = Hash(item);
+			var h = Hash(key);
 			for (var n = nodes[h]; n != null; n = n.Next)
-				if (Comparer.Equals(n.Item, item)) return true;
+				if (Comparer.Equals(n.Key, key)) return true;
 			return false;
 		}
 
-		public bool Add(T item)
+		public bool Add(TKey key)
 		{
-			var h = Hash(item);
+			var h = Hash(key);
 			for (var n = nodes[h]; n != null; n = n.Next)
-				if (Comparer.Equals(n.Item, item)) return false;
+				if (Comparer.Equals(n.Key, key)) return false;
 
-			nodes[h] = new Node { Item = item, Next = nodes[h] };
+			nodes[h] = new Node { Key = key, Next = nodes[h] };
 			++Count;
 			return true;
 		}
 
-		public bool Remove(T item)
+		public bool Remove(TKey key)
 		{
-			var h = Hash(item);
+			var h = Hash(key);
 			for (ref var n = ref nodes[h]; n != null; n = ref n.Next)
-				if (Remove(ref n, item)) return true;
+				if (Remove(ref n, key)) return true;
 			return false;
 		}
 
-		bool Remove(ref Node n, T item)
+		bool Remove(ref Node n, TKey key)
 		{
-			if (!Comparer.Equals(n.Item, item)) return false;
+			if (!Comparer.Equals(n.Key, key)) return false;
 			n = n.Next;
 			--Count;
 			return true;
