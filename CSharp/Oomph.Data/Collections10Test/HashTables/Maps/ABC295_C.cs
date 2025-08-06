527a40c94163d81fe2adcea194bffa7dc6ab5dc3
sakapon <git@saka-pon.net>
2025-08-06 11:36:34 +0900

    Update test

--- CSharp/Oomph.Data/Collections10Test/HashTables/Maps/ABC295_C.cs
+++ CSharp/Oomph.Data/Collections10Test/HashTables/Maps/ABC295_C.cs
@@ -2,6 +2,7 @@ using Oomph.Data.Collections10Lib.HashTables.Chain.v201;
 
 namespace Collections10Test.HashTables.Maps
 {
+	// 50 ñúåè
 	// Test: https://atcoder.jp/contests/abc295/tasks/abc295_c
 	class ABC295_C
 	{
@@ -12,7 +13,7 @@ namespace Collections10Test.HashTables.Maps
 			var n = int.Parse(Console.ReadLine());
 			var a = Read();
 
-			var map = new FixedChainHashMap<int, int>(20);
+			var map = new ChainHashMap<int, int>();
 			foreach (var x in a) map[x]++;
 			return map.Sum(p => p.Value / 2);
 		}
