3dac82f11fb885cd59ffb1bcd02b6c05dea4c6fb
Keiho Sakapon <git@saka-pon.net>
2025-08-09 01:03:45 +0900

    Add test

---
+++ CSharp/Oomph.Data/Collections10Test/HashTables/Sets/ChainHashSetTest.cs
@@ -0,0 +1,58 @@
+﻿using Oomph.Data.Collections10Lib.HashTables.Chain.v301;
+
+namespace Collections10Test.HashTables.Sets
+{
+	class ChainHashSetTest
+	{
+		static readonly Random Random = new();
+		static void Main()
+		{
+			Set_Equal();
+
+			var n = 1 << 20;
+			var data_int = Enumerable.Range(0, n).ToArray();
+			TestHelper.MeasureTime(() => Set_Ops(data_int));
+			data_int = Array.ConvertAll(data_int, _ => Random.Next());
+			TestHelper.MeasureTime(() => Set_Ops(data_int));
+		}
+
+		public static void Set_Equal()
+		{
+			var n = 1 << 10;
+			var data = Enumerable.Range(0, n).Select(_ => Random.Next(n)).ToArray();
+
+			var u = new bool[n];
+			var set = new ChainHashSet<int>();
+
+			foreach (var i in data)
+			{
+				u[i] = true;
+				set.Add(i);
+			}
+			TestHelper.AreEqual(u.Count(b => b), set.Count);
+
+			for (var i = 0; i < n; i++)
+			{
+				TestHelper.AreEqual(u[i], set.Contains(i));
+			}
+
+			foreach (var i in data)
+			{
+				set.Remove(i);
+			}
+			TestHelper.AreEqual(0, set.Count);
+			TestHelper.AreEqual(0, set.ToArray().Length);
+		}
+
+		public static void Set_Ops<T>(T[] data)
+		{
+			var set = new ChainHashSet<T>();
+			foreach (var item in data)
+				set.Add(item);
+			foreach (var item in data)
+				set.Contains(item);
+			foreach (var item in data)
+				set.Remove(item);
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/Collections10Test/TestHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Collections10Test
+{
+	public static class TestHelper
+	{
+		public static void AreEqual<T>(T expected, T actual)
+		{
+			if (!EqualityComparer<T>.Default.Equals(expected, actual))
+				throw new InvalidOperationException($"expected: {expected}, actual: {actual}");
+		}
+
+		public static void AreEqual<T>(IEnumerable<T> expected, IEnumerable<T> actual)
+		{
+			if (!expected.SequenceEqual(actual))
+				throw new InvalidOperationException($"expected: {expected}, actual: {actual}");
+		}
+
+		public static void MeasureTime(Action test)
+		{
+			var startTime = DateTime.Now;
+			test();
+			var span = DateTime.Now - startTime;
+			Console.WriteLine($"{span.TotalSeconds:F3} sec");
+		}
+	}
+}
