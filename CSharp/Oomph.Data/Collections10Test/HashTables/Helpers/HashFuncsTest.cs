7f9a71798942def5e1a9726c8a0693ba448ef464
Keiho Sakapon <git@saka-pon.net>
2025-08-09 13:00:23 +0900

    Refactor

--- CSharp/Oomph.Data/Collections10Test/HashTables/Helpers/HashFuncsTest.cs
+++ CSharp/Oomph.Data/Collections10Test/HashTables/Helpers/HashFuncsTest.cs
@@ -7,10 +7,10 @@ namespace Collections10Test.HashTables.Helpers
 		static void Main()
 		{
 			var h = HashFuncs.CreateMultiplicationDouble(HashFuncs.MagicDouble);
-			AreEqual(67U, h(123456, 14));
+			TestHelper.AreEqual(67U, h(123456, 14));
 
 			h = HashFuncs.CreateMultiplication(HashFuncs.MagicUInt32);
-			AreEqual(67U, h(123456, 14));
+			TestHelper.AreEqual(67U, h(123456, 14));
 
 			TestHash(h);
 			TestHash(HashFuncs.CreateMultiplicationDouble());
@@ -22,12 +22,6 @@ namespace Collections10Test.HashTables.Helpers
 		{
 			var seq = Enumerable.Range(700, 600).Select(i => h((uint)i, 10));
 			Console.WriteLine(seq.Distinct().Count());
-		}
-
-		public static void AreEqual<T>(T expected, T actual)
-		{
-			if (!EqualityComparer<T>.Default.Equals(expected, actual))
-				throw new InvalidOperationException("Not Equal");
 		}
 	}
 }
