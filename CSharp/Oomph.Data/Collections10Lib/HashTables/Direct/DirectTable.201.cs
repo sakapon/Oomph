9df2f048c3b6fd7b50f20219f64b5fe1d9fe9b5a
Keiho Sakapon <git@saka-pon.net>
2025-08-16 22:21:01 +0900

    Copy classes

---
+++ CSharp/Oomph.Data/Collections10Lib/HashTables/Direct/DirectTable.201.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Oomph.Data.Collections10Lib.HashTables.Direct.v201
+{
+	// Add, Contains, Remove, Item[]
+	// Count, DefaultValue, Clear
+	public class DirectMap<TValue>
+	{
+		readonly bool[] b;
+		readonly TValue[] values;
+		public int Count { get; private set; }
+		public TValue DefaultValue { get; }
+
+		public DirectMap(int n, TValue v0 = default)
+		{
+			b = new bool[n];
+			values = new TValue[n];
+			DefaultValue = v0;
+		}
+
+		public void Clear()
+		{
+			Array.Clear(b);
+			Count = 0;
+		}
+
+		public TValue this[int key]
+		{
+			get => b[key] ? values[key] : DefaultValue;
+			set
+			{
+				if (!b[key]) ++Count;
+				b[key] = true;
+				values[key] = value;
+			}
+		}
+
+		public bool Contains(int key)
+		{
+			return b[key];
+		}
+
+		public bool Add(int key, TValue value)
+		{
+			if (b[key]) return false;
+			b[key] = true;
+			values[key] = value;
+			++Count;
+			return true;
+		}
+
+		public bool Remove(int key)
+		{
+			if (!b[key]) return false;
+			b[key] = false;
+			--Count;
+			return true;
+		}
+	}
+
+	// Add, Contains, Remove
+	// Count, Clear
+	public class DirectSet
+	{
+		readonly DirectMap<bool> map;
+		public DirectSet(int n) => map = new(n);
+		public int Count => map.Count;
+		public void Clear() => map.Clear();
+		public bool Contains(int key) => map.Contains(key);
+		public bool Add(int key) => map.Add(key, false);
+		public bool Remove(int key) => map.Remove(key);
+	}
+}
