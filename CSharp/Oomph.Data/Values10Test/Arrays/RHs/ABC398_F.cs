926a0968323239acec7843c96eee82fc4bf72696
Keiho Sakapon <git@saka-pon.net>
2025-08-18 18:32:06 +0900

    Add test

---
+++ CSharp/Oomph.Data/Values10Test/Arrays/RHs/ABC398_F.cs
@@ -0,0 +1,34 @@
+using Oomph.Data.Values10Lib.Arrays.RHs.v100;
+
+namespace Values10Test.Arrays.RHs
+{
+	// Test: https://atcoder.jp/contests/abc398/tasks/abc398_f
+	class ABC398_F
+	{
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var s = Console.ReadLine().ToCharArray();
+			var n = s.Length;
+
+			var sr = s.ToArray();
+			Array.Reverse(sr);
+
+			var rh = new RollingHashArray<char>(s);
+			var rhr = new RollingHashArray<char>(sr);
+
+			for (int i = 0; i < n; i++)
+			{
+				if (!IsPalindrome(i, n)) continue;
+				return new string(s) + new string(sr[(n - i)..]);
+			}
+			throw new InvalidOperationException();
+
+			bool IsPalindrome(int l, int r)
+			{
+				var len = (r - l) / 2;
+				return rh.GetHashCode(l, l + len) == rhr.GetHashCode(n - r, n - r + len);
+			}
+		}
+	}
+}
