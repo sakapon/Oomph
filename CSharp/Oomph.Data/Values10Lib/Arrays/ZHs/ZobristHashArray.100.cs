ab885507b4db7f3993f6049284c007ce47d2cb64
Keiho Sakapon <git@saka-pon.net>
2025-08-20 00:05:10 +0900

    Add Zobrist hash classes

---
+++ CSharp/Oomph.Data/Values10Lib/Arrays/ZHs/ZobristHashArray.100.cs
@@ -0,0 +1,37 @@
+﻿// mutable array
+
+namespace Oomph.Data.Values10Lib.Arrays.ZHs.v100
+{
+	public class ZobristHashArray<T>
+	{
+		const int B = (int)(2654435769L - (1L << 32));
+		static int Hash(T o) => o?.GetHashCode() ?? 0;
+
+		readonly T[] a;
+		public T[] Raw => a;
+		readonly int n;
+		int h;
+
+		public ZobristHashArray(T[] a)
+		{
+			this.a = a;
+			n = a.Length;
+
+			for (int i = 0; i < n; ++i)
+				h ^= B * Hash(a[i]);
+		}
+
+		public T this[int i]
+		{
+			get => a[i];
+			set
+			{
+				h ^= B * Hash(a[i]);
+				a[i] = value;
+				h ^= B * Hash(a[i]);
+			}
+		}
+
+		public override int GetHashCode() => h;
+	}
+}
---
+++ CSharp/Oomph.Data/Values10Lib/Arrays/ZHs/ZobristHashSet.100.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Oomph.Data.Values10Lib.Arrays.ZHs.v100
+{
+	public class ZobristHashSet<T> : HashSet<T>
+	{
+		const int B = (int)(2654435769L - (1L << 32));
+		static int Hash(T o) => o?.GetHashCode() ?? 0;
+
+		int h;
+		public override int GetHashCode() => h;
+
+		public new bool Add(T item)
+		{
+			var r = base.Add(item);
+			if (r) h ^= B * Hash(item);
+			return r;
+		}
+
+		public new bool Remove(T item)
+		{
+			var r = base.Remove(item);
+			if (r) h ^= B * Hash(item);
+			return r;
+		}
+	}
+}
