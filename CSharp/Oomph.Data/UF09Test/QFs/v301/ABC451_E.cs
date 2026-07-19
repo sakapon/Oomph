a7181d21dbfb9898b7c91309d99f06103f0e52a2
Keiho Sakapon <git@saka-pon.net>
2026-07-19 17:36:37 +0900

    Add test

---
+++ CSharp/Oomph.Data/UF09Test/QFs/v301/ABC451_E.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.QFs.v301;
+
+namespace UF09Test.QFs.v301
+{
+	// Test: https://atcoder.jp/contests/abc451/tasks/abc451_e
+	class ABC451_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+		static bool Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var a = Array.ConvertAll(new bool[n - 1], _ => Read());
+
+			a = a.Append(new int[0])
+				.Select((row, i) => new int[i + 1].Concat(row).ToArray())
+				.ToArray();
+
+			int GetDistance(int u, int v)
+			{
+				if (u > v) (u, v) = (v, u);
+				return a[u][v];
+			}
+
+			var es = new List<(int d, int u, int v)>();
+			for (int i = 0; i < n; i++)
+				for (int j = i + 1; j < n; j++)
+					es.Add((a[i][j], i, j));
+
+			var uf = new QuickFind(n);
+			foreach (var (d, u, v) in es.OrderBy(e => e.d))
+			{
+				if (uf.AreSame(u, v)) continue;
+
+				foreach (var uu in uf.Find(u).Items)
+					foreach (var vv in uf.Find(v).Items)
+					{
+						var expected = GetDistance(uu, vv);
+						var actual = GetDistance(uu, u) + d + GetDistance(v, vv);
+						if (expected != actual) return false;
+					}
+
+				uf.Union(u, v);
+			}
+			return true;
+		}
+	}
+}
---
+++ CSharp/Oomph.Data/UF09Test/UFs/v301/L3/ABC447_E.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oomph.Data.UF09Lib.UFs.v301;
+
+namespace UF09Test.UFs.v301.L3
+{
+	// Test: https://atcoder.jp/contests/abc447/tasks/abc447_e
+	class ABC447_E
+	{
+		const long M = 998244353;
+		const long MHalf = (M + 1) / 2;
+
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var p = 1L;
+			for (int i = 0; i < m; i++)
+				p = p * 2 % M;
+
+			var r = 0L;
+			var uf = new UnionFind(n + 1);
+
+			Array.Reverse(es);
+			foreach (var (u, v) in es)
+			{
+				if (uf.GroupsCount != 3)
+					uf.Union(u, v);
+				else if (!uf.AreSame(u, v))
+					r = (r + p) % M;
+
+				p = p * MHalf % M;
+			}
+			return r;
+		}
+	}
+}
